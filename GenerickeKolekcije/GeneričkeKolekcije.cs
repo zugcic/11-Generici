74fafd5fd6e325541e8c2b09ec4179c25a65c45a
Stjepan Zugcic <stzugcic@vsite.hr>
2016-12-15 21:03:17 +0100

    Riješili smo Generičke kolekcije

--- GenerickeKolekcije/GeneričkeKolekcije.cs
+++ GenerickeKolekcije/GeneričkeKolekcije.cs
@@ -9,14 +9,17 @@ namespace Vsite.CSharp
         public static void TipskiNesigurnaKolekcija()
         {
             ArrayList osobe = new ArrayList();
-            // TODO: U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+			//  U listu 'osobe' dodati dvije osobe s imenima "Ivica" i "Marica" te proizvoljnim datumima rođenja. Prevesti program, pokrenuti ga i provjeriti ispis.
+			Osoba ivica = new Osoba("Ivica", new DateTime(1873, 11, 2));
+			Osoba marica = new Osoba("Marica", new DateTime(1875, 11, 2));
+			osobe.Add(ivica);
+			osobe.Add(marica);
 
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
 
-            // TODO: U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
-
+			// TODO: U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
+			osobe.Add(3);
             foreach (Osoba osoba in osobe)
                 Console.WriteLine(osoba.Ime);
         }
@@ -24,9 +27,20 @@ namespace Vsite.CSharp
         public static void GeneričkaKolekcija()
         {
             List<Osoba> osobe = new List<Osoba>();
-            // TODO: Ponoviti dodavananja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+			// TODO: Ponoviti dodavananja i ispise iz gornje metode. Prevesti program, pokrenuti ga i provjeriti ispis.
+			Osoba ivica = new Osoba("Ivica", new DateTime(1873, 11, 2));
+			Osoba marica = new Osoba("Marica", new DateTime(1875, 11, 2));
+			osobe.Add(ivica);
+			osobe.Add(marica);
 
-        }
+			foreach (Osoba osoba in osobe)
+				Console.WriteLine(osoba.Ime);
+
+			// TODO: U listu 'osobe' dodati objekt nekog drugog tipa. Prevesti program, pokrenuti ga i provjeriti ispis.
+			//osobe.Add(3);
+			foreach (Osoba osoba in osobe)
+				Console.WriteLine(osoba.Ime);
+		}
 
         static void Main(string[] args)
         {
